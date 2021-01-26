b6b2b39adb4fb2bd3edd438d0b7e3eb8eb49b348
Christian Gram Kalhauge <christian@kalhauge.dk>
2021-01-26 10:33:30 +0100

    Add first test

---
+++ src/test/java/example/SetupTest.java
@@ -0,0 +1,15 @@
+package example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SetupTest {
+
+    @Test
+    @DisplayName("Should run")
+    void shouldRun() {
+        assertTrue(true);
+    }
+}
